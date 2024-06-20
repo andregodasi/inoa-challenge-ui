ee9482ee6a2cb105e8705433a2dec6faf0b26832
andregodasi <andre.godasi@gmail.com>
2024-06-20 03:28:04 -0300

    fix: eslint

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,10 +52,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...propsData }) => (
+          <ChevronLeft className="h-4 w-4" {...propsData} />
+        ),
+        IconRight: ({ ...propsData }) => (
+          <ChevronRight className="h-4 w-4" {...propsData} />
+        ),
       }}
       {...props}
     />
