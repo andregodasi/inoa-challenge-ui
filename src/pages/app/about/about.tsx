af710cfc6a137bbca2234704953b68a3a0d6b9f9
andregodasi <andre.godasi@gmail.com>
2024-06-20 02:47:18 -0300

    feat: create about page

---
+++ src/pages/app/about/about.tsx
@@ -0,0 +1,28 @@
+export function About() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold tracking-tight">Sobre o projeto</h1>
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <div className="text-base leading-7 lg:max-w-lg">
+                <p className="text-base font-semibold leading-7">Desafio</p>
+                <h1 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+                  Dashboard de Ativos B3
+                </h1>
+                <div className="max-w-xl">
+                  <p className="mt-6">
+                    Desafio proposto pela Inoa, onde o objetivo é criar um
+                    dashboard de ativos da B3, onde o usuário possa visualizar
+                    informações sobre o mercado financeiro.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
